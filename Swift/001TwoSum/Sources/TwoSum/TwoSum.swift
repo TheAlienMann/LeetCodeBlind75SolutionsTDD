1765dac17365d8ba53f2e5e9498a51487e248047
Mohamad Rahmani <rahmanipour.mohammad@gmail.com>
2021-10-20 23:19:26 +0330

    fixed the unit tests' names :/

--- Swift/001TwoSum/Sources/TwoSum/TwoSum.swift
+++ Swift/001TwoSum/Sources/TwoSum/TwoSum.swift
@@ -2,15 +2,17 @@ import Foundation
 
 class TwoSum {
   func twoSum(_ nums: [Int], _ target: Int) -> [Int] {
-		var previouslyEncountered = [Int: Int]()
-		for (idx, value) in nums.enumerated() {
-			let diff = target - value
-			if let exists = previouslyEncountered[diff] {
-				return [exists, idx]
-			} else {
-				previouslyEncountered[value] = idx
-			}
-		}
+    guard nums.count > 1 else { return [] }
+
+    var previouslyEncountered = [Int: Int]()
+    for (idx, value) in nums.enumerated() {
+      let diff = target - value
+      if let exists = previouslyEncountered[diff] {
+        return [exists, idx]
+      } else {
+        previouslyEncountered[value] = idx
+      }
+    }
     return []
   }
 }
--- Swift/001TwoSum/Tests/TwoSumTests/TwoSumTests.swift
+++ Swift/001TwoSum/Tests/TwoSumTests/TwoSumTests.swift
@@ -14,26 +14,26 @@ final class TwoSumTests: XCTestCase {
     super.tearDown()
   }
 
-  func testTwoSum_whenNums_isEmpty() {
+  func testTwoSum_whenNumsIsEmpty_shouldReturnAnEmptyArray() {
     XCTAssertEqual(sut.twoSum([], 0), [])
   }
 
-	func testTwoSum_whenNums_hasLessThanTwoItems() {
-		XCTAssertEqual(sut.twoSum([1], 9), [])
-	}
+  func testTwoSum_whenNumsHasLessThanTwoItems_shouldReturnAnEmptyArray() {
+    XCTAssertEqual(sut.twoSum([1], 9), [])
+  }
 
-	func testTwoSum_whenNums_criteriaIsNotMet() {
-		XCTAssertEqual(sut.twoSum([1, 1, 1, 1], 0), [])
-	}
+  func testTwoSum_whenNumsHasMoreThanAndEqualToTwoItemsButDoesntMetTheCriteria_shouldReturnAnEmptyArray() {
+    XCTAssertEqual(sut.twoSum([1, 1, 1, 1], 0), [])
+  }
 
-	func testTwoSum_whenNums_criteriaIsMet() {
-		XCTAssertEqual(sut.twoSum([2, 7, 11, 15], 9), [0, 1])
-	}
+  func testTwoSum_whenNumsMeetTheCriteria_shouldReturnTheIndciesOfTheElements() {
+    XCTAssertEqual(sut.twoSum([2, 7, 11, 15], 9), [0, 1])
+  }
 
   static var allTests = [
-		("testTwoSum_whenNums_isEmpty", testTwoSum_whenNums_isEmpty),
-		("testTwoSum_whenNums_hasLessThanTwoItems", testTwoSum_whenNums_hasLessThanTwoItems),
-		("testTwoSum_whenNums_criteriaIsNotMet", testTwoSum_whenNums_criteriaIsNotMet),
-		("testTwoSum_whenNums_criteriaIsMet", testTwoSum_whenNums_criteriaIsMet)
+    ("testTwoSum_whenNumsIsEmpty_shouldReturnAnEmptyArray", testTwoSum_whenNumsIsEmpty_shouldReturnAnEmptyArray),
+    ("testTwoSum_whenNumsHasLessThanTwoItems_shouldReturnAnEmptyArray", testTwoSum_whenNumsHasLessThanTwoItems_shouldReturnAnEmptyArray),
+    ("testTwoSum_whenNumsHasMoreThanAndEqualToTwoItemsButDoesntMetTheCriteria_shouldReturnAnEmptyArray", testTwoSum_whenNumsHasMoreThanAndEqualToTwoItemsButDoesntMetTheCriteria_shouldReturnAnEmptyArray),
+    ("testTwoSum_whenNumsMeetTheCriteria_shouldReturnTheIndciesOfTheElements", testTwoSum_whenNumsMeetTheCriteria_shouldReturnTheIndciesOfTheElements),
   ]
 }
