f4a6ce27c71ca8bfe355726af7805171c9ed735c
Mohamad Rahmani <rahmanipour.mohammad@gmail.com>
2021-10-30 22:51:33 +0330

    done

--- Swift/012RotateImage/Sources/RotateImage/RotateImage.swift
+++ Swift/012RotateImage/Sources/RotateImage/RotateImage.swift
@@ -1,7 +1,31 @@
 import Foundation
 
 class RotateImage {
-  func rotate(_: inout [[Int]]) {
-    // it takes some time to solve this one :|
-  }
+	var input = [[Int]]()
+	func rotate(_ matrix: inout [[Int]]) {
+		let numberOfRows = matrix.count
+		let numberOfColumns = matrix[0].count
+
+		// swapping rows and columns
+		for row in 0 ..< numberOfRows {
+			for col in row ..< numberOfColumns {
+				let temp = matrix[row][col]
+				matrix[row][col] = matrix[col][row]
+				matrix[col][row] = temp
+			}
+		}
+
+		// reversing each row
+		for row in 0 ..< numberOfRows {
+			var start = 0, end = numberOfColumns - 1
+			while start <= end {
+				let temp = matrix[row][start]
+				matrix[row][start] = matrix[row][end]
+				matrix[row][end] = temp
+				start += 1
+				end -= 1
+			}
+		}
+		input = matrix
+	}
 }
--- Swift/012RotateImage/Tests/RotateImageTests/RotateImageTests.swift
+++ Swift/012RotateImage/Tests/RotateImageTests/RotateImageTests.swift
@@ -2,23 +2,50 @@
 import XCTest
 
 final class RotateImageTests: XCTestCase {
-  var sut: RotateImage!
+	var sut: RotateImage!
 
-  override func setUp() {
-    super.setUp()
-    sut = RotateImage()
-  }
+	override func setUp() {
+		super.setUp()
+		sut = RotateImage()
+	}
 
-  override func tearDown() {
-    sut = nil
-    super.tearDown()
-  }
+	override func tearDown() {
+		sut = nil
+		super.tearDown()
+	}
 
-  func testExample() {
-    XCTAssertEqual(1 + 1, 2)
-  }
+	func testExample() {
+		var matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+		let output = [[7, 4, 1], [8, 5, 2], [9, 6, 3]]
+		sut.rotate(&matrix)
+		XCTAssertEqual(sut.input, output)
+	}
 
-  static var allTests = [
-    ("testExample", testExample),
-  ]
+	func testExample2() {
+		var matrix = [[5, 1, 9, 11], [2, 4, 8, 10], [13, 3, 6, 7], [15, 14, 12, 16]]
+		let output = [[15, 13, 2, 5], [14, 3, 4, 1], [12, 6, 8, 9], [16, 7, 10, 11]]
+		sut.rotate(&matrix)
+		XCTAssertEqual(sut.input, output)
+	}
+
+	func testExample3() {
+		var matrix = [[1]]
+		let output = [[1]]
+		sut.rotate(&matrix)
+		XCTAssertEqual(sut.input, output)
+	}
+
+	func testExample4() {
+		var matrix = [[1, 2], [3, 4]]
+		let output = [[3, 1], [4, 2]]
+		sut.rotate(&matrix)
+		XCTAssertEqual(sut.input, output)
+	}
+
+	static var allTests = [
+		("testExample", testExample),
+		("testExample2", testExample2),
+		("testExample3", testExample3),
+		("testExample4", testExample4),
+	]
 }
