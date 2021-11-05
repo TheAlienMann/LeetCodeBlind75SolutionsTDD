fc6e9d3c0e02fadf20a12bafe6b6e34e3669c7f2
Mohamad Rahmani <rahmanipour.mohammad@gmail.com>
2021-11-05 19:38:59 +0330

    Trading Stocks done, now I am pretty much rich!

--- Swift/030BestTimeToBuyAndSellStock/Sources/BestTimeToBuyAndSellStock/BestTimeToBuyAndSellStock.swift
+++ Swift/030BestTimeToBuyAndSellStock/Sources/BestTimeToBuyAndSellStock/BestTimeToBuyAndSellStock.swift
@@ -1,9 +1,19 @@
 import Foundation
 
 class BestTimeToBuyAndSellStock {
-  func maxProfit(_ prices: [Int]) -> Int {
-    guard !prices.isEmpty else { return 0 }
-
-    return 0
-  }
+	func maxProfit(_ prices: [Int]) -> Int {
+		guard !prices.isEmpty else { return 0 }
+		var left = 0, right = 1
+		var currentMaxProfit = 0
+		while right < prices.count {
+			if prices[left] < prices[right] {
+				let profit = prices[right] - prices[left]
+				currentMaxProfit = max(currentMaxProfit, profit)
+			} else {
+				left = right
+			}
+			right += 1
+		}
+		return currentMaxProfit
+	}
 }
--- Swift/030BestTimeToBuyAndSellStock/Tests/BestTimeToBuyAndSellStockTests/BestTimeToBuyAndSellStockTests.swift
+++ Swift/030BestTimeToBuyAndSellStock/Tests/BestTimeToBuyAndSellStockTests/BestTimeToBuyAndSellStockTests.swift
@@ -2,23 +2,33 @@
 import XCTest
 
 final class BestTimeToBuyAndSellStockTests: XCTestCase {
-  var sut: BestTimeToBuyAndSellStock!
+	var sut: BestTimeToBuyAndSellStock!
 
-  override func setUp() {
-    super.setUp()
-    sut = BestTimeToBuyAndSellStock()
-  }
+	override func setUp() {
+		super.setUp()
+		sut = BestTimeToBuyAndSellStock()
+	}
 
-  override func tearDown() {
-    sut = nil
-    super.tearDown()
-  }
+	override func tearDown() {
+		sut = nil
+		super.tearDown()
+	}
 
-  func testExample1() {
-    XCTAssertEqual(sut.maxProfit([]), 0)
-  }
+	func testExample1() {
+		XCTAssertEqual(sut.maxProfit([]), 0)
+	}
 
-  static var allTests = [
-    ("testExample1", testExample1),
-  ]
+	func testExample2() {
+		XCTAssertEqual(sut.maxProfit([7, 1, 5, 3, 6, 4]), 5)
+	}
+
+	func testExample3() {
+		XCTAssertEqual(sut.maxProfit([7, 6, 4, 3, 1]), 0)
+	}
+
+	static var allTests = [
+		("testExample1", testExample1),
+		("testExample2", testExample2),
+		("testExample3", testExample3),
+	]
 }
