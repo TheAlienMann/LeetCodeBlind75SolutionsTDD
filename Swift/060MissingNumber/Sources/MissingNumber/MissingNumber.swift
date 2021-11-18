d617921d627719ad93239266d98ddfb7ce426ff9
Mohamad Rahmani <rahmanipour.mohammad@gmail.com>
2021-11-18 22:48:23 +0330

    missig number swift done

--- Swift/060MissingNumber/Sources/MissingNumber/MissingNumber.swift
+++ Swift/060MissingNumber/Sources/MissingNumber/MissingNumber.swift
@@ -2,9 +2,7 @@ import Foundation
 
 class MissingNumber {
 	func missingNumber(_ nums: [Int]) -> Int {
-		guard nums.count > 1 else { return 1 } // this is just temprorality
-
-
-		return 1
+   let sumUpToTheCountOfNums = nums.count * (nums.count + 1) / 2
+    return sumUpToTheCountOfNums - nums.reduce(0, +)
 	}
 }
--- Swift/060MissingNumber/Tests/MissingNumberTests/MissingNumberTests.swift
+++ Swift/060MissingNumber/Tests/MissingNumberTests/MissingNumberTests.swift
@@ -18,7 +18,16 @@ final class MissingNumberTests: XCTestCase {
 		XCTAssertEqual(sut.missingNumber([0]), 1)
 	}
 
+  func testExample2() {
+    XCTAssertEqual(sut.missingNumber([3, 0, 1]), 2)
+  }
+
+  func testExample3() {
+    XCTAssertEqual(sut.missingNumber([0, 1]), 2)
+  }
 	static var allTests = [
 		("testExample", testExample),
+    ("testExample2", testExample2),
+    ("testExample3", testExample3),
 	]
 }
