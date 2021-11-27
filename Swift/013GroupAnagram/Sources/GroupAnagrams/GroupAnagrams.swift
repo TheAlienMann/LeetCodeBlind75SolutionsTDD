78dee5706b94e7b9e1406ba0bc236b75e03e4d0b
Mohamad Rahmani <rahmanipour.mohammad@gmail.com>
2021-11-27 23:28:45 +0330

    group anagram nummber zwei :|

--- Swift/013GroupAnagram/Sources/GroupAnagrams/GroupAnagrams.swift
+++ Swift/013GroupAnagram/Sources/GroupAnagrams/GroupAnagrams.swift
@@ -3,6 +3,17 @@ import Foundation
 class GroupAnagrams {
 	func groupAnagrams(_ strs: [String]) -> [[String]] {
 		guard !strs.isEmpty else { return [] }
-		return []
+
+		var res = [[Int]: [String]]()
+
+		for string in strs {
+			var countOfEachCharacter = Array(repeating: 0, count: 26)
+			for char in string {
+				countOfEachCharacter[Int(UnicodeScalar(String(char))!.value - UnicodeScalar(String("a"))!.value)] += 1
+			}
+			print(countOfEachCharacter)
+			res[countOfEachCharacter]?.append(string)
+		}
+		return res.values.map { $0 }
 	}
 }
