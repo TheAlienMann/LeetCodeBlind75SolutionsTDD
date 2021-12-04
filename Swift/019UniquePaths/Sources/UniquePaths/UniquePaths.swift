94c025b5cbac59f305974149d451968b9e97d80d
Mohamad Rahmani <rahmanipour.mohammad@gmail.com>
2021-12-04 23:34:42 +0330

    unique paths nummer eins

--- Swift/019UniquePaths/Sources/UniquePaths/UniquePaths.swift
+++ Swift/019UniquePaths/Sources/UniquePaths/UniquePaths.swift
@@ -2,6 +2,14 @@ import Foundation
 
 class UniquePaths {
   func uniquePaths(_ m: Int, _ n: Int) -> Int {
-    return m * n
+    var row = Array(repeating: 1, count: n)
+    for _ in 0..<m-1 {
+      var newRow = Array(repeating: 1, count: n)
+      for j in stride(from: n-2, to: -1, by: -1) {
+        newRow[j] = newRow[j+1] + row[j]
+      }
+      row = newRow
+    }
+    return row[0]
   }
 }
