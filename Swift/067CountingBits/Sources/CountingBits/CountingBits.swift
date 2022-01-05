efd33c8b86f0b6c29f2fd92d3fbfe7439efec11d
Mohamad Rahmani <rahmanipour.mohammad@gmail.com>
2022-01-05 19:51:09 +0330

    counting bits done

--- Swift/067CountingBits/Sources/CountingBits/CountingBits.swift
+++ Swift/067CountingBits/Sources/CountingBits/CountingBits.swift
@@ -1,5 +1,28 @@
 public class CountingBits {
 	func countBits(_ num: Int) -> [Int] {
-		return [num]
+		// var dp = Array(repeating: 0, count: num + 1)
+		// var offset = 1
+		// for idx in 1 ... num {
+		// 	if offset * 2 == idx {
+		// 		offset = idx
+		// 	}
+		// 	dp[idx] = 1 + dp[dp.index(dp.startIndex, offsetBy: idx - offset)]
+		// }
+		// return dp
+		var result = [Int]()
+		for idx in 0 ... num {
+			result.append(numberOfOnes(idx))
+		}
+		return result
+	}
+
+	private func numberOfOnes(_ num: Int) -> Int {
+		var num = num
+		var count = 0
+		while num > 0 {
+			count += num & 1
+			num >>= 1
+		}
+		return count
 	}
 }
