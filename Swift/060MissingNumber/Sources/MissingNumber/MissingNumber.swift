74b35f3134c7788b7f1fb2db04b720bf7263fd1f
Mohamad Rahmani <rahmanipour.mohammad@gmail.com>
2022-01-07 01:11:35 +0330

    missing number zwei 🤔

--- Swift/060MissingNumber/Sources/MissingNumber/MissingNumber.swift
+++ Swift/060MissingNumber/Sources/MissingNumber/MissingNumber.swift
@@ -2,7 +2,7 @@ import Foundation
 
 class MissingNumber {
 	func missingNumber(_ nums: [Int]) -> Int {
-   let sumUpToTheCountOfNums = nums.count * (nums.count + 1) / 2
-    return sumUpToTheCountOfNums - nums.reduce(0, +)
+		let sumUpToTheCountOfNums = nums.count * (nums.count + 1) / 2
+		return sumUpToTheCountOfNums - nums.reduce(0, +)
 	}
 }
