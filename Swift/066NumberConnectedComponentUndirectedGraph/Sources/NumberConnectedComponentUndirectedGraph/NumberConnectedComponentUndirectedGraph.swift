539af44e987edc0d03c44153a1e685b08ecbf628
Mohamad Rahmani <rahmanipour.mohammad@gmail.com>
2022-01-06 22:14:40 +0330

    number of connected components in undirected graph (union algorithm) done

--- Swift/066NumberConnectedComponentUndirectedGraph/Sources/NumberConnectedComponentUndirectedGraph/NumberConnectedComponentUndirectedGraph.swift
+++ Swift/066NumberConnectedComponentUndirectedGraph/Sources/NumberConnectedComponentUndirectedGraph/NumberConnectedComponentUndirectedGraph.swift
@@ -1,6 +1,36 @@
-public struct NumberConnectedComponentUndirectedGraph {
-    public private(set) var text = "Hello, World!"
+public class NumberConnectedComponentUndirectedGraph {
+	func countComponents(_ edges: [[Int]], _ num: Int) -> Int {
+		var parent = Array(0 ..< num)
+		var rank = Array(repeating: 1, count: num)
 
-    public init() {
-    }
+		func find(_ node: Int) -> Int {
+			var result = node
+			while result != parent[result] {
+				parent[result] = parent[parent[result]]
+				result = parent[result]
+			}
+			return result
+		}
+
+		func union(_ node1: Int, _ node2: Int) -> Int {
+			let p1 = find(node1), p2 = find(node2)
+			if p1 == p2 {
+				return 0
+			}
+			if rank[p2] > rank[p1] {
+				parent[p1] = p2
+				rank[p2] += rank[p1]
+			} else {
+				parent[p2] = p1
+				rank[p1] += rank[p2]
+			}
+			return 1
+		}
+
+		var result = num
+		for item in edges {
+			result -= union(item.first!, item.last!)
+		}
+		return result
+	}
 }
