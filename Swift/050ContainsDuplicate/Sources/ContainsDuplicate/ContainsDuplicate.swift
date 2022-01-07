d0fe66842ece03975c26b47f0e0b6fe01890c297
Mohamad Rahmani <rahmanipour.mohammad@gmail.com>
2022-01-08 02:16:50 +0330

    does it contain duplicate items, done.

--- Swift/050ContainsDuplicate/Sources/ContainsDuplicate/ContainsDuplicate.swift
+++ Swift/050ContainsDuplicate/Sources/ContainsDuplicate/ContainsDuplicate.swift
@@ -2,9 +2,16 @@ import Foundation
 
 class ContainsDuplicate {
 	func containsDuplicate(_ nums: [Int]) -> Bool {
-		guard !nums.isEmpty else { fatalError("Expected the input to have at least one element.") }
-		guard Set(nums).count != 1 else { return true }
-		// on going.
+		// guard !nums.isEmpty else { fatalError("Expected the input to have at least one element.") }
+		// return Set(nums).count != nums.count
+
+		var setOfNums = Set<Int>()
+		for num in nums {
+			if setOfNums.contains(num) {
+				return true
+			}
+			setOfNums.insert(num)
+		}
 		return false
 	}
 }
