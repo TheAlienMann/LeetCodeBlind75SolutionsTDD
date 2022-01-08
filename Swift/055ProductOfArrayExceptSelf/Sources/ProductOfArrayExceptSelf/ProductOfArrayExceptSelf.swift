29f2b3edbdcc303a07dcb28a847773a7aa27dcd3
Mohamad Rahmani <rahmanipour.mohammad@gmail.com>
2022-01-08 14:21:04 +0330

    product of array except self, done

--- Swift/055ProductOfArrayExceptSelf/Sources/ProductOfArrayExceptSelf/ProductOfArrayExceptSelf.swift
+++ Swift/055ProductOfArrayExceptSelf/Sources/ProductOfArrayExceptSelf/ProductOfArrayExceptSelf.swift
@@ -1,3 +1,18 @@
 struct ProductOfArrayExceptSelf {
-  var text = "Hello, World!"
+	func productExceptSelf(_ nums: [Int]) -> [Int] {
+		guard !nums.isEmpty else { return [] }
+		var result = Array(repeating: 1, count: nums.count)
+		var prefix = 1
+		for idx in 0 ..< nums.count {
+			result[idx] = prefix
+			prefix *= nums[idx]
+		}
+
+		var postfix = 1
+		for idx in stride(from: nums.count - 1, through: 0, by: -1) {
+			result[idx] *= postfix
+			postfix *= nums[idx]
+		}
+		return result
+	}
 }
--- Swift/055ProductOfArrayExceptSelf/Tests/ProductOfArrayExceptSelfTests/ProductOfArrayExceptSelfTests.swift
+++ Swift/055ProductOfArrayExceptSelf/Tests/ProductOfArrayExceptSelfTests/ProductOfArrayExceptSelfTests.swift
@@ -2,14 +2,28 @@
 import XCTest
 
 final class ProductOfArrayExceptSelfTests: XCTestCase {
-  func testExample() {
-    // This is an example of a functional test case.
-    // Use XCTAssert and related functions to verify your tests produce the correct
-    // results.
-    XCTAssertEqual(ProductOfArrayExceptSelf().text, "Hello, World!")
-  }
+	var sut: ProductOfArrayExceptSelf!
 
-  static var allTests = [
-    ("testExample", testExample),
-  ]
+	override func setUp() {
+		super.setUp()
+		sut = ProductOfArrayExceptSelf()
+	}
+
+	override func tearDown() {
+		sut = nil
+		super.tearDown()
+	}
+
+	func testExample() {
+		XCTAssertEqual(sut.productExceptSelf([1, 2, 3, 4]), [24, 12, 8, 6])
+	}
+
+	func testExample2() {
+		XCTAssertEqual(sut.productExceptSelf([-1, 1, 0, -3, 3]), [0, 0, 9, 0, 0])
+	}
+
+	static var allTests = [
+		("testExample", testExample),
+		("testExample2", testExample2),
+	]
 }
