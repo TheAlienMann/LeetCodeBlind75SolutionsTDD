cc10543c058dfd1ba54604bcdf64c32e2c8ea2f6
Mohamad Rahmani <rahmanipour.mohammad@gmail.com>
2022-01-08 15:09:27 +0330

    reorder linkedlist, done

--- Swift/037ReorderList/Sources/ReorderList/ReorderList.swift
+++ Swift/037ReorderList/Sources/ReorderList/ReorderList.swift
@@ -1,5 +1,34 @@
 import Foundation
 
+class ReorderList {
+	func reorderList(_ head: ListNode?) {
+		guard head != nil else { return }
+		var slow = head, fast = head!.next
+		while fast != nil, fast!.next != nil {
+			slow = slow!.next
+			fast = fast!.next!.next
+		}
+		var secondPortionHead = slow!.next
+		var previous: ListNode?
+		slow!.next = nil
+		while secondPortionHead != nil {
+			let temp = secondPortionHead!.next
+			secondPortionHead!.next = previous
+			previous = secondPortionHead
+			secondPortionHead = temp
+		}
+		var firstPortion = head
+		secondPortionHead = previous
+		while secondPortionHead != nil {
+			let temp1 = firstPortion!.next
+			let temp2 = secondPortionHead!.next
+			firstPortion!.next = secondPortionHead
+			secondPortionHead!.next = temp1
+			(firstPortion, secondPortionHead) = (temp1, temp2)
+		}
+	}
+}
+
 public class ListNode {
 	public var val: Int
 	public var next: ListNode?
@@ -16,12 +45,5 @@ public class ListNode {
 	public init(_ val: Int, _ next: ListNode?) {
 		self.val = val
 		self.next = next
-	}
-}
-
-class ReorderList {
-	func reorderList(_ head: ListNode?) {
-		guard head != nil else { return }
-		// got work to do, will come back to this soon.
 	}
 }
