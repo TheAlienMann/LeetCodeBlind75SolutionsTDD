f293b40f61c783e2cf23d84d510f612770440a68
Mohamad Rahmani <rahmanipour.mohammad@gmail.com>
2022-03-06 18:43:36 +0330

    Modified and fixed some issue regarding following Linting

--- Swift/001TwoSum/Sources/TwoSum/TwoSum.swift
+++ Swift/001TwoSum/Sources/TwoSum/TwoSum.swift
@@ -1,18 +1,18 @@
 import Foundation
 
 class TwoSum {
-  func twoSum(_ nums: [Int], _ target: Int) -> [Int] {
-    guard nums.count > 1 else { return [] }
+	func twoSum(_ nums: [Int], _ target: Int) -> [Int] {
+		guard nums.count > 1 else { return [] }
 
-    var previouslyEncountered = [Int: Int]()
-    for (idx, value) in nums.enumerated() {
-      let diff = target - value
-      if let exists = previouslyEncountered[diff] {
-        return [exists, idx]
-      } else {
-        previouslyEncountered[value] = idx
-      }
-    }
-    return []
-  }
+		var previouslyEncountered = [Int: Int]()
+		for (idx, value) in nums.enumerated() {
+			let diff = target - value
+			if let exists = previouslyEncountered[diff] {
+				return [exists, idx]
+			} else {
+				previouslyEncountered[value] = idx
+			}
+		}
+		return []
+	}
 }
--- Swift/001TwoSum/Tests/TwoSumTests/TwoSumTests.swift
+++ Swift/001TwoSum/Tests/TwoSumTests/TwoSumTests.swift
@@ -1,39 +1,43 @@
-@testable import TwoSum
 import XCTest
+@testable import TwoSum
 
 final class TwoSumTests: XCTestCase {
-  var sut: TwoSum!
+	var sut: TwoSum!
 
-  override func setUp() {
-    super.setUp()
-    sut = TwoSum()
-  }
+	override func setUp() {
+		super.setUp()
+		sut = TwoSum()
+	}
 
-  override func tearDown() {
-    sut = nil
-    super.tearDown()
-  }
+	override func tearDown() {
+		sut = nil
+		super.tearDown()
+	}
 
-  func testTwoSum_whenNumsIsEmpty_shouldReturnAnEmptyArray() {
-    XCTAssertEqual(sut.twoSum([], 0), [])
-  }
+	func test_twoSum_whenNumsIsEmpty_shouldReturnAnEmptyArray() {
+		XCTAssertEqual(sut.twoSum([], 0), [])
+	}
 
-  func testTwoSum_whenNumsHasLessThanTwoItems_shouldReturnAnEmptyArray() {
-    XCTAssertEqual(sut.twoSum([1], 9), [])
-  }
+	func test_twoSum_whenNumsHasLessThanTwoItems_shouldReturnAnEmptyArray() {
+		XCTAssertEqual(sut.twoSum([1], 9), [])
+	}
 
-  func testTwoSum_whenNumsHasMoreThanAndEqualToTwoItemsButDoesntMetTheCriteria_shouldReturnAnEmptyArray() {
-    XCTAssertEqual(sut.twoSum([1, 1, 1, 1], 0), [])
-  }
+	func test_twoSum_whenNumsHasMoreThanAndEqualToTwoItemsButDoesntMetTheCriteria_shouldReturnAnEmptyArray() {
+		XCTAssertEqual(sut.twoSum([1, 1, 1, 1], 0), [])
+	}
 
-  func testTwoSum_whenNumsMeetTheCriteria_shouldReturnTheIndciesOfTheElements() {
-    XCTAssertEqual(sut.twoSum([2, 7, 11, 15], 9), [0, 1])
-  }
+	func test_twoSum_whenNumsMeetTheCriteria_shouldReturnTheIndciesOfTheElements() {
+		XCTAssertEqual(sut.twoSum([2, 7, 11, 15], 9), [0, 1])
+	}
 
-  static var allTests = [
-    ("testTwoSum_whenNumsIsEmpty_shouldReturnAnEmptyArray", testTwoSum_whenNumsIsEmpty_shouldReturnAnEmptyArray),
-    ("testTwoSum_whenNumsHasLessThanTwoItems_shouldReturnAnEmptyArray", testTwoSum_whenNumsHasLessThanTwoItems_shouldReturnAnEmptyArray),
-    ("testTwoSum_whenNumsHasMoreThanAndEqualToTwoItemsButDoesntMetTheCriteria_shouldReturnAnEmptyArray", testTwoSum_whenNumsHasMoreThanAndEqualToTwoItemsButDoesntMetTheCriteria_shouldReturnAnEmptyArray),
-    ("testTwoSum_whenNumsMeetTheCriteria_shouldReturnTheIndciesOfTheElements", testTwoSum_whenNumsMeetTheCriteria_shouldReturnTheIndciesOfTheElements),
-  ]
+	static var allTests = [
+		("test_twoSum_whenNumsIsEmpty_shouldReturnAnEmptyArray",
+		 test_twoSum_whenNumsIsEmpty_shouldReturnAnEmptyArray),
+		("test_twoSum_whenNumsHasLessThanTwoItems_shouldReturnAnEmptyArray",
+		 test_twoSum_whenNumsHasLessThanTwoItems_shouldReturnAnEmptyArray),
+		("test_twoSum_whenNumsHasMoreThanAndEqualToTwoItemsButDoesntMetTheCriteria_shouldReturnAnEmptyArray",
+		 test_twoSum_whenNumsHasMoreThanAndEqualToTwoItemsButDoesntMetTheCriteria_shouldReturnAnEmptyArray),
+		("test_twoSum_whenNumsMeetTheCriteria_shouldReturnTheIndciesOfTheElements",
+		 test_twoSum_whenNumsMeetTheCriteria_shouldReturnTheIndciesOfTheElements),
+	]
 }
