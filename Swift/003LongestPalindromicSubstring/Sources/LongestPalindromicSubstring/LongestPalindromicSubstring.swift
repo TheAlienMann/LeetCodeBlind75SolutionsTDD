ff230f94e250df79947119e8ab53a0ec080103d1
Mohamad Rahmani <rahmanipour.mohammad@gmail.com>
2022-03-06 18:43:36 +0330

    Modified and fixed some issue regarding following Linting

--- Swift/003LongestPalindromicSubstring/Sources/LongestPalindromicSubstring/LongestPalindromicSubstring.swift
+++ Swift/003LongestPalindromicSubstring/Sources/LongestPalindromicSubstring/LongestPalindromicSubstring.swift
@@ -1,39 +1,39 @@
 import Foundation
 
 class LongestPalindromicSubstring {
-  func longestPalindrome(_ str: String) -> String {
-    guard str.count > 1 else { return str }
-    var result: [Character]!
-    var lengthSoFar = 0
-    let strArray = Array(str)
-    for i in 0 ..< strArray.count {
-      var left = i, right = i
-      while left >= 0,
-            right < strArray.count,
-            strArray[left] == strArray[right]
-      {
-        if right - left + 1 > lengthSoFar {
-          result = Array(strArray[left ... right])
-          lengthSoFar = right - left + 1
-        }
-        left -= 1
-        right += 1
-      }
+	func longestPalindrome(_ str: String) -> String {
+		guard str.count > 1 else { return str }
+		var result: [Character]!
+		var lengthSoFar = 0
+		let strArray = Array(str)
+		for idx in 0 ..< strArray.count {
+			var left = idx, right = idx
+			while left >= 0,
+			      right < strArray.count,
+			      strArray[left] == strArray[right]
+			{
+				if right - left + 1 > lengthSoFar {
+					result = Array(strArray[left ... right])
+					lengthSoFar = right - left + 1
+				}
+				left -= 1
+				right += 1
+			}
 
-      left = i
-      right = i + 1
-      while left >= 0,
-            right < strArray.count,
-            strArray[left] == strArray[right]
-      {
-        if right - left + 1 > lengthSoFar {
-          result = Array(strArray[left ... right])
-          lengthSoFar = right - left + 1
-        }
-        left -= 1
-        right += 1
-      }
-    }
-    return String(result)
-  }
+			left = idx
+			right = idx + 1
+			while left >= 0,
+			      right < strArray.count,
+			      strArray[left] == strArray[right]
+			{
+				if right - left + 1 > lengthSoFar {
+					result = Array(strArray[left ... right])
+					lengthSoFar = right - left + 1
+				}
+				left -= 1
+				right += 1
+			}
+		}
+		return String(result)
+	}
 }
--- Swift/003LongestPalindromicSubstring/Tests/LongestPalindromicSubstringTests/LongestPalindromicSubstringTests.swift
+++ Swift/003LongestPalindromicSubstring/Tests/LongestPalindromicSubstringTests/LongestPalindromicSubstringTests.swift
@@ -1,34 +1,37 @@
-@testable import LongestPalindromicSubstring
 import XCTest
+@testable import LongestPalindromicSubstring
 
 final class LongestPalindromicSubstringTests: XCTestCase {
-  var sut: LongestPalindromicSubstring!
+	var sut: LongestPalindromicSubstring!
 
-  override func setUp() {
-    super.setUp()
-    sut = LongestPalindromicSubstring()
-  }
+	override func setUp() {
+		super.setUp()
+		sut = LongestPalindromicSubstring()
+	}
 
-  override func tearDown() {
-    sut = nil
-    super.tearDown()
-  }
+	override func tearDown() {
+		sut = nil
+		super.tearDown()
+	}
 
-  func testLongestPalindrome_whenLengthOfStringIsLessThanTwo_shouldReturnTheString() {
-    XCTAssertEqual(sut.longestPalindrome("e"), "e")
-  }
+	func test_longestPalindrome_whenLengthOfStringIsLessThanTwo_shouldReturnTheString() {
+		XCTAssertEqual(sut.longestPalindrome("e"), "e")
+	}
 
-  func testLongestPalindrome_whenLengthIsOkay_lengthOfTheExpectedPalindromeIsOdd_shouldReturnIt() {
-    XCTAssertEqual(sut.longestPalindrome("babad"), "bab")
-  }
+	func test_longestPalindrome_whenLengthIsOkay_lengthOfTheExpectedPalindromeIsOdd_shouldReturnIt() {
+		XCTAssertEqual(sut.longestPalindrome("babad"), "bab")
+	}
 
-  func testLongestPalindrome_whenLengthIsOkay_lengthOfTheExpectedPalindromeIsEven_shouldReturnIt() {
-    XCTAssertEqual(sut.longestPalindrome("cbbd"), "bb")
-  }
+	func test_longestPalindrome_whenLengthIsOkay_lengthOfTheExpectedPalindromeIsEven_shouldReturnIt() {
+		XCTAssertEqual(sut.longestPalindrome("cbbd"), "bb")
+	}
 
-  static var allTests = [
-    ("testLongestPalindrome_whenLengthOfStringIsLessThanTwo_shouldReturnTheString", testLongestPalindrome_whenLengthOfStringIsLessThanTwo_shouldReturnTheString),
-    ("testLongestPalindrome_whenLengthIsOkay_lengthOfTheExpectedPalindromeIsOdd_shouldReturnIt", testLongestPalindrome_whenLengthIsOkay_lengthOfTheExpectedPalindromeIsOdd_shouldReturnIt),
-    ("testLongestPalindrome_whenLengthIsOkay_lengthOfTheExpectedPalindromeIsEven_shouldReturnIt", testLongestPalindrome_whenLengthIsOkay_lengthOfTheExpectedPalindromeIsEven_shouldReturnIt),
-  ]
+	static var allTests = [
+		("test_longestPalindrome_whenLengthOfStringIsLessThanTwo_shouldReturnTheString",
+		 test_longestPalindrome_whenLengthOfStringIsLessThanTwo_shouldReturnTheString),
+		("test_longestPalindrome_whenLengthIsOkay_lengthOfTheExpectedPalindromeIsOdd_shouldReturnIt",
+		 test_longestPalindrome_whenLengthIsOkay_lengthOfTheExpectedPalindromeIsOdd_shouldReturnIt),
+		("test_longestPalindrome_whenLengthIsOkay_lengthOfTheExpectedPalindromeIsEven_shouldReturnIt",
+		 test_longestPalindrome_whenLengthIsOkay_lengthOfTheExpectedPalindromeIsEven_shouldReturnIt),
+	]
 }
