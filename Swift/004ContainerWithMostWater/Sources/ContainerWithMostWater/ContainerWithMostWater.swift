8e50eea33664165f3f43a7ee2c04a90ad3260c55
Mohamad Rahmani <rahmanipour.mohammad@gmail.com>
2022-03-08 11:50:36 +0330

    Linting and making tests names descriptive, which they should have been

--- Swift/004ContainerWithMostWater/Sources/ContainerWithMostWater/ContainerWithMostWater.swift
+++ Swift/004ContainerWithMostWater/Sources/ContainerWithMostWater/ContainerWithMostWater.swift
@@ -1,18 +1,18 @@
 import Foundation
 
 class ContainerWithMostWater {
-  func maxArea(_ heights: [Int]) -> Int {
-    var left = 0
-    var right = heights.count - 1
-    var maxArea = 0
-    while left < right {
-      maxArea = max(maxArea, min(heights[left], heights[right]) * (right - left))
-      if heights[left] <= heights[right] {
-        left += 1
-      } else {
-        right -= 1
-      }
-    }
-    return maxArea
-  }
+	func maxArea(_ heights: [Int]) -> Int {
+		var left = 0
+		var right = heights.count - 1
+		var maxArea = 0
+		while left < right {
+			maxArea = max(maxArea, min(heights[left], heights[right]) * (right - left))
+			if heights[left] <= heights[right] {
+				left += 1
+			} else {
+				right -= 1
+			}
+		}
+		return maxArea
+	}
 }
--- Swift/004ContainerWithMostWater/Tests/ContainerWithMostWaterTests/ContainerWithMostWaterTests.swift
+++ Swift/004ContainerWithMostWater/Tests/ContainerWithMostWaterTests/ContainerWithMostWaterTests.swift
@@ -1,29 +1,35 @@
-@testable import ContainerWithMostWater
 import XCTest
+@testable import ContainerWithMostWater
 
 final class ContainerWithMostWaterTests: XCTestCase {
-  var sut: ContainerWithMostWater!
+	var sut: ContainerWithMostWater!
 
-  override func setUp() {
-    super.setUp()
-    sut = ContainerWithMostWater()
-  }
+	override func setUp() {
+		super.setUp()
+		sut = ContainerWithMostWater()
+	}
 
-  override func tearDown() {
-    sut = nil
-    super.tearDown()
-  }
+	override func tearDown() {
+		sut = nil
+		super.tearDown()
+	}
 
-  func testExample() {
-    XCTAssertEqual(sut.maxArea([]), 0)
-  }
+	func test_maxArea_whenHeightsIsEmpty_shouldReturnZero() {
+		XCTAssertEqual(sut.maxArea([]), 0)
+	}
 
-  func testMaxArea() {
-    XCTAssertEqual(sut.maxArea([1, 8, 6, 2, 5, 4, 8, 3, 7]), 49)
-  }
+	func test_maxArea_whenHeightsHasJustOneItem_shouldReturnZero() {
+		XCTAssertEqual(sut.maxArea([10]), 0)
+	}
 
-  static var allTests = [
-    ("testExample", testExample),
-    ("testMaxArea", testMaxArea),
-  ]
+	func test_maxArea_whenHeightsHasMoreThanTwoItems_shouldReturnCalculatedAmount() {
+		XCTAssertEqual(sut.maxArea([1, 8, 6, 2, 5, 4, 8, 3, 7]), 49)
+	}
+
+	static var allTests = [
+		("test_maxArea_whenHeightsIsEmpty_shouldReturnZero", test_maxArea_whenHeightsIsEmpty_shouldReturnZero),
+		("test_maxArea_whenHeightsHasJustOneItem_shouldReturnZero", test_maxArea_whenHeightsHasJustOneItem_shouldReturnZero),
+		("test_maxArea_whenHeightsHasMoreThanTwoItems_shouldReturnCalculatedAmount",
+		 test_maxArea_whenHeightsHasMoreThanTwoItems_shouldReturnCalculatedAmount),
+	]
 }
