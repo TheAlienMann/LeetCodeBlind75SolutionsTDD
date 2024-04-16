3cf5b95d315f9b75c4c4e7ed6022bd5f599e7dbc
Mohamad Rahmani <rahmanipour.mohammad@gmail.com>
2024-04-16 15:37:13 +0100

    add TwoSum for Java

---
+++ Java/001TwoSum/TwoSum/src/test/java/TwoSumTests.java
@@ -0,0 +1,12 @@
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class TwoSumTests {
+    @Test
+    @DisplayName("test two sum")
+    void testTwoSum() throws Exception {
+        assertTrue(false);
+    }
+}
